03aefcd88fc0f960d0fcea53f2e7dc1ce9e3dc52
Uttam Biswas <110588388+ubiswasOCode@users.noreply.github.com>
2022-12-02 18:38:40 +0530

    new changes
    new changes

--- SEO_Checker/density/views.py
+++ SEO_Checker/density/views.py
@@ -4,7 +4,6 @@ import requests
 from bs4 import BeautifulSoup
 import metadata_parser
 import re
-import string
 
 
 def Density_Check(request):
@@ -15,13 +14,15 @@ def Density_Check(request):
     if request.method == "POST":
         url = request.POST.get('url')
         if (url):
+
             context = Density(url)
         else:
-            context =WordDensity()
+            context =WordDensity(wrdsrch)
 
         # context = WordDensity(wrdsrch)
 
         context['url']=url
+
 
     return render(request, 'Density.html', context)
 
@@ -60,12 +61,13 @@ def Density(url):
 
       ### Keywords
     keyword=page.get_metadata("keywords")
-
+    print(keyword,"-------------------------keywordss")
 
 
     #Get Description
     meta_desc=page.get_metadata("description")
-
+    print(meta_desc,"---------------Description")
+    # desc_list=list(meta_desc)
 
 
     for para in soup.find_all("p"):
@@ -157,9 +159,7 @@ def Density(url):
 
     #After Removing
     One_Word = list(set(one))
-    chars = re.escape(string.punctuation)
-    One_Word=re.sub(r"[^a-zA-Z0-9]+", ' ', str(One_Word))
-
+    print(one,"---------------------one")
     if "C++" in One_Word:
         One_Word.remove("C++")
 
@@ -169,12 +169,15 @@ def Density(url):
         Total_word1=len(one)
         density=round((int(len(results))/int(Total_word1))*100,2)
         data[kwrd] = {"freq": len(results), "density": density}
+        # data[kwrd] = len(results)
+        # print(results,"-----------------------results")
+
 
 
     #Two Word Frequency and Density calculate
     data1={}
     Two_Word=list(set(two))
-
+    print(two,"--------------------------Two")
     for Kword1 in Two_Word:
         results1 = soup.body.find_all(string=re.compile('.*{0}.*'.format(Kword1)), recursive=True)
         Total_word2=len(two)
@@ -192,8 +195,7 @@ def Density(url):
     print(three,"-----------Three List")
     Convert_set=set(three)
     Three_word=list(set(Convert_set))
-    chars1 = re.escape(string.punctuation)
-    Three_word=re.sub(r"[^a-zA-Z0-9]+", ' ', str(Three_word))
+    print(Three_word,"===============Three Word")
 
     data2={}
     for Kword2 in Three_word:
@@ -225,10 +227,9 @@ def Density(url):
     return  {"data":data, "data1": data1, "data2":data2, "data3":data3}
 
 
-def WordDensity(request):
-    wrdsrch=""
-    req = requests.get(wrdsrch)
-    convert=list(req.split(' '))
+def WordDensity(wrdsrch):
+
+    convert=list(wrdsrch.split(' '))
 
     frequency={}
 
@@ -253,13 +254,18 @@ def WordDensity(request):
     print(frequency)
 
 
+    # print(convert)
+    # three_word=list(map(' '.join, zip(convert[:-2], convert[2:])))
+    # print(three_word)
+
+
     three=[]
     for i in range(len(convert) - 2):
         three.append(convert[i] + ' ' + convert[i+2])
     print(three)
 
 
-    return render(request,"Density.html",{"frequency":frequency})
+    return {"frequency":frequency}
 
 
 
